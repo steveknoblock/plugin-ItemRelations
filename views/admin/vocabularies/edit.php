2ed0de8892344536f7817dd947b804a2075cf5ce
Steve Knoblock <steve.knoblock@gmail.com>
2013-07-18 13:28:58 -0400

    Upgrade modifications

--- views/admin/vocabularies/edit.php
+++ views/admin/vocabularies/edit.php
@@ -32,19 +32,19 @@ relation you want them to convey.</p>
 <?php foreach ($this->properties as $property): ?>
         <tr>
             <td><?php echo $property->label; ?></td>
-            <td><?php echo __v()->formTextarea("property_description[{$property->id}]", $property->description, array('cols' => 50, 'rows' => 2)); ?></td>
-            <td><?php echo __v()->formCheckbox("property_delete[{$property->id}]") ?></td>
+            <td><?php echo get_view()->formTextarea("property_description[{$property->id}]", $property->description, array('cols' => 50, 'rows' => 2)); ?></td>
+            <td><?php echo get_view()->formCheckbox("property_delete[{$property->id}]") ?></td>
         </tr>
 <?php endforeach; ?>
         <tr class="new-property">
-            <td><?php echo __v()->formText("new_property_label[]", null, array('size' => 30)); ?></td>
-            <td><?php echo __v()->formTextarea("new_property_description[]", null, array('cols' => 50, 'rows' => 2)); ?></td>
+            <td><?php echo get_view()->formText("new_property_label[]", null, array('size' => 30)); ?></td>
+            <td><?php echo get_view()->formTextarea("new_property_description[]", null, array('cols' => 50, 'rows' => 2)); ?></td>
             <td><span style="color:#ccc;">n/a</span></td>
         </tr>
     </tbody>
 </table>
-<?php echo __v()->formButton('add_property', 'Add a Property', array('id' => 'add-property')); ?>
-<?php echo __v()->formSubmit('submit_edit_vocabulary', 'Submit', array('class' => 'submit submit-medium')); ?>
+<?php echo get_view()->formButton('add_property', 'Add a Property', array('id' => 'add-property')); ?>
+<?php echo get_view()->formSubmit('submit_edit_vocabulary', 'Submit', array('class' => 'submit submit-medium')); ?>
 </form>
 </div>
 <?php echo foot(); ?>
